924029c45c46281802e70bd1a04be65e3828db1d
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-08-26 02:37:05 +0900

    Fixed to use where.

--- chapter03/02/excercise06.hs
+++ chapter03/02/excercise06.hs
@@ -1,4 +1,7 @@
 module Main where
     sortByLength :: [[a]] -> [[a]]
     sortByLength []         = []
-    sortByLength (pivot:xs) = sortByLength [x | x <- xs, length x < length pivot] ++ [pivot] ++ sortByLength [x | x <- xs, length x >= length pivot]
+    sortByLength (pivot:xs) = sortByLength shorter ++ [pivot] ++ sortByLength longer
+                                where
+                                  shorter = [x | x <- xs, length x <  length pivot]
+                                  longer  = [x | x <- xs, length x >= length pivot]
