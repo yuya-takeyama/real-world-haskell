6d4607b13cf1693544558e009dca2e0704b6cdd6
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-10-07 02:18:15 +0900

    Solved takeWhile using foldr.

--- chapter04/Exercises/takeWhile.hs
+++ chapter04/Exercises/takeWhile.hs
@@ -2,3 +2,9 @@ takeWhile' :: (a -> Bool) -> [a] -> [a]
 takeWhile' _ []                 = []
 takeWhile' p (x:xs) | p x       = x : takeWhile' p xs
                     | otherwise = []
+
+takeWhile'' :: (a -> Bool) -> [a] -> [a]
+takeWhile'' p xs = foldr f [] xs
+  where
+    f x acc | p x       = x : acc
+            | otherwise = []
