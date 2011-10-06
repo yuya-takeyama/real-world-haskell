d87845543aa67f77b097b4c8164b81200e32a5f9
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-10-07 01:04:12 +0900

    Handles errors.

--- chapter04/Exercises/asInt_fold.hs
+++ chapter04/Exercises/asInt_fold.hs
@@ -1,9 +1,11 @@
 import Data.Char (digitToInt)
 
 asInt_fold :: [Char] -> Int
+asInt_fold ""                 = error "Empty string"
 asInt_fold (x:xs) | x == '-'  = negate (asInt' xs)
                   | otherwise = asInt' (x:xs)
   where
+    asInt' "" = error "Invalid format"
     asInt' xs = foldl f 0 xs
       where
         f x y = x * 10 + digitToInt y
